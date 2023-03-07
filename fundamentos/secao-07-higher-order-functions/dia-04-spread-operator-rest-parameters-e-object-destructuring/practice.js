966b0398875536df8890c66cb8ae5669404c9d8a
Filipe Santos <filipe_bruno91@hotmail.com>
2023-03-07 12:30:05 -0300

    studying

--- fundamentos/secao-07-higher-order-functions/dia-04-spread-operator-rest-parameters-e-object-destructuring/practice.js
+++ fundamentos/secao-07-higher-order-functions/dia-04-spread-operator-rest-parameters-e-object-destructuring/practice.js
@@ -10,3 +10,23 @@ const fruitSalad = (fruit, additional) => {
 };
 
 console.log(fruitSalad(specialFruit, additionalItens));
+
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+}
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL'
+}
+
+const informations = {...user, ... jobInfos};
+
+const { name, age, nationality, profession, squad, squadInitials} = informations;
+
+const text = `Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}.`
+
+console.log(text);
