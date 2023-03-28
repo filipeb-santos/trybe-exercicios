cedb4e9bd4eaf54de6c13f56b45743d3561de4ee
Filipe Santos <filipe_bruno91@hotmail.com>
2023-03-28 16:42:33 -0300

    initial commit

---
+++ front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/nome-da-aplicacao/src/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          {'Filipe Santos'}
+        </h1>
+        <p>
+          {'Engenheiro Civil, aprendiz de programador!'}
+        </p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>DOM</li>
+          <li>HTML e CSS</li>
+          <li>JS Assíncrono</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default About;
