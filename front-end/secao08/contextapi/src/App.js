5eb87997a02b2284ce5fb1183ee12e2873649274
Filipe Santos <filipe_bruno91@hotmail.com>
2023-05-28 19:47:27 -0300

    useContext

---
+++ front-end/secao08/contextapi/src/App.js
@@ -0,0 +1,15 @@
+import ThemeProvider from './context/ThemeProvider';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Image from './components/Image';
+import React from 'react';
+
+export default function App() {
+  return (
+    <ThemeProvider>
+        <Header />
+        <Image />
+        <Footer />
+    </ThemeProvider>
+  );
+}
---
+++ front-end/secao08/contextapi/src/components/Image.js
@@ -0,0 +1,12 @@
+  import React from "react";
+
+  function Image() {
+    return(
+      <div>
+        <img src="/trybe.svg" alt="trybe logo" />
+      </div>
+    )
+  }
+
+  export default Image;
+   
